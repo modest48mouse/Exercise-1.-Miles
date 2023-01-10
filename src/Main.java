b0e2d145f9dc6214e8bb14ed840461702c03d029
Alina test <modest48mouse@gmail.com>
2023-01-10 15:17:15 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2000;
+        int mile = amount / 20;
+
+        System.out.println("Итоговое количество миль: " + mile);
+    }
+}
